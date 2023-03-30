e4362da1773ba3a11a603f08f786052144975200
YURI HONG <dbfl720@gmail.com>
2023-03-30 21:43:39 +0900

    종합 문제 - JSTL - quiz05

--- src/main/java/com/quiz/lesson05/bo/WeatherHistoryBO.java
+++ src/main/java/com/quiz/lesson05/bo/WeatherHistoryBO.java
@@ -1,5 +1,13 @@
 package com.quiz.lesson05.bo;
 
-public class WeatherHistoryBO {
+import org.springframework.stereotype.Service;
 
+import com.quiz.lesson05.model.WeatherHistory;
+
+@Service
+public class WeatherHistoryBO {
+	
+	public void addWeather(WeatherHistory weatherHistory) {
+		;
+	}
 }
---
+++ src/main/java/com/quiz/lesson05/model/WeatherHistory.java
@@ -0,0 +1,75 @@
+package com.quiz.lesson05.model;
+
+import java.util.Date;
+
+public class WeatherHistory {
+
+		private int id;
+		private String date;
+		private String weather;
+		private float temperatures;
+		private float precipitation;
+		private String microDust;
+		private float windSpeed;
+		private Date createdAt;
+		private Date updatedAt;
+		
+		
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public String getDate() {
+			return date;
+		}
+		public void setDate(String date) {
+			this.date = date;
+		}
+		public String getWeather() {
+			return weather;
+		}
+		public void setWeather(String weather) {
+			this.weather = weather;
+		}
+		public float getTemperatures() {
+			return temperatures;
+		}
+		public void setTemperatures(float temperatures) {
+			this.temperatures = temperatures;
+		}
+		public float getPrecipitation() {
+			return precipitation;
+		}
+		public void setPrecipitation(float precipitation) {
+			this.precipitation = precipitation;
+		}
+		public String getMicroDust() {
+			return microDust;
+		}
+		public void setMicroDust(String microDust) {
+			this.microDust = microDust;
+		}
+		public float getWindSpeed() {
+			return windSpeed;
+		}
+		public void setWindSpeed(float windSpeed) {
+			this.windSpeed = windSpeed;
+		}
+		public Date getCreatedAt() {
+			return createdAt;
+		}
+		public void setCreatedAt(Date createdAt) {
+			this.createdAt = createdAt;
+		}
+		public Date getUpdatedAt() {
+			return updatedAt;
+		}
+		public void setUpdatedAt(Date updatedAt) {
+			this.updatedAt = updatedAt;
+		}
+		
+		
+		
+}
